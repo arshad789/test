e4da932560d6da3cb2ad7b8f5359a0742826868f
arshad <arshadahmed789@gmail.com>
2024-04-28 23:44:45 +0530

    addd

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-testterraformtest-bucket"
-    key            = "test/"
+    key            = "test"
     region         = "us-east-1"
     dynamodb_table = "my-dynamodbtest-table"
     encrypt        = true
