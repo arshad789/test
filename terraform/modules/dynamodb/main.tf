a117831805e69450f6eade214d030cbe3c9961e7
arshad <arshadahmed789@gmail.com>
2024-04-28 22:44:30 +0530

    asdff

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -4,8 +4,22 @@ variable "table_name" {
   description = "The name of the DynamoDB table"
 }
 
-resource "aws_dynamodb_table" "example" {
-  name         = var.table_name
-  billing_mode = "PAY_PER_REQUEST"
-}
 
+
+
+resource "aws_dynamodb_table" "example" {
+  name           = "var.table_name"
+  billing_mode   = "PAY_PER_REQUEST"
+  
+  attribute {
+    name = "id"
+    type = "S"  # S for string, N for number
+  }
+
+  key_schema {
+    attribute_name = "id"
+    key_type       = "HASH"
+  }
+
+ 
+}
