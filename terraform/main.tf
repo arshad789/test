294fc383d1dc41f8fa834b13ad46940c18281714
arshad <arshadahmed789@gmail.com>
2024-04-28 21:41:02 +0530

    adada

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+variable "instance_type" {}
+variable "ami" {}
+
+
+
 module "ec2_instance" {
   source        = "github.com/arshad789/test/terraform/modules/ec2"
   instance_type = var.instance_type
